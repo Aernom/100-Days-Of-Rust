b2d5ef8022a56f14883b5ab505585aaa2c085561
Aernom <andrea.montorio@gmail.com>
2024-04-11 11:45:42 +0200

    day 1 completed

--- Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/src/main.rs
+++ Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/src/main.rs
@@ -1,27 +1,18 @@
 use std::io;
 
 fn main() {
-
     loop {
+        let mut buffer = String::new();
 
-    println!("Please input your age in years!");
-
-    let mut guess = String::new();
-
-    
         io::stdin()
-            .read_line(&mut guess)
-            .expect("Could not read age. Make you an integer is used.");
-        
-        let guess: u32 =  match guess.trim().parse() {
-            Ok(num) => num, 
-            Err(_) => continue,
-        };
+            .read_line(&mut buffer)
+            .expect("Error reading from std in");
 
-        let age: u32 = guess * 365;
-
-        println!("You are roughly {age} days old!")
-
+        if let Ok(age) = buffer.trim().parse::<u32>() {
+            println!("Your Age in days is {}", age * 365);
+            break;
+        } else {
+            println!("Not a valid age!");
+        }
     }
-
 }
