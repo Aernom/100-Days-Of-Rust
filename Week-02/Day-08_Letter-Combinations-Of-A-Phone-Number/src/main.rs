0b087f6f91b46ad9ae0abb5a365b177d3382796c
Aernom <andrea.montorio@gmail.com>
2024-04-14 22:09:12 +0200

    working on day 8

---
+++ Week-02/Day-08_Letter-Combinations-Of-A-Phone-Number/src/main.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+fn main() {
+    let digit_map = HashMap::from([
+        ('2', vec!['a', 'b', 'c']),
+        ('3', vec!['d', 'e', 'f']),
+        ('4', vec!['g', 'h', 'i']),
+        ('5', vec!['j', 'k', 'l']),
+        ('6', vec!['m', 'n', 'o']),
+        ('7', vec!['p', 'q', 'r', 's']),
+        ('8', vec!['t', 'u', 'v']),
+        ('9', vec!['w', 'x', 'y', 'z']),
+    ]);
+}
