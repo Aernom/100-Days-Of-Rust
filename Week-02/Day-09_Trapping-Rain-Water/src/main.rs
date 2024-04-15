32822476af4b5063c444395b160885539da37de9
Aernom <andrea.montorio@gmail.com>
2024-04-16 00:17:00 +0200

    working on day 9

--- Week-02/Day-09_Trapping-Rain-Water/src/main.rs
+++ Week-02/Day-09_Trapping-Rain-Water/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+fn trapped_water(section: &[u32]) -> u32 {
+    let start = *section.first().unwrap_or(&0);
+    let end = *section.last().unwrap_or(&0);
+
+    if start == 0 || end == 0 {
+        return 0;
+    }
+
+    let peak = start.min(end);
+    let max_area = peak * (section.len() as u32 - 1);
 }
+
+const EL_1: [u32; 12] = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+const EL_2: [u32; 12] = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
